bc34801b830d4b6a13ea4f46e2e9dbffbcd26c35
Suman Pramanick <pramanicksuman64@gmail.com>
2026-06-20 22:19:32 +0530

    update to direct backend URL

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://customer-support-ticket-system-mlyl.vercel.app/api',
 });
 
 axiosInstance.interceptors.request.use((config) => {
